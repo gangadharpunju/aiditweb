12f63859d51a8362a4a86e8f89bf82b597a7a759
sivananda <cvasia549@gmail.com>
2024-06-27 11:23:24 +0530

    Cybersecurity changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,36 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-const path = require("path")
+// const path = require("path")
 
-const nextConfig = {
-    reactStrictMode:true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, "styles")]
-    }
-}
+// const nextConfig = {
+//     reactStrictMode:true,
+//     sassOptions: {
+//         includePaths: [path.join(__dirname, "styles")]
+//     }
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    images: {
+      domains: ['example.com'],
+    },
+    async redirects() {
+      return [
+        {
+          source: '/old-path',
+          destination: '/new-path',
+          permanent: true,
+        },
+      ]
+    },
+    webpack(config, { isServer }) {
+      if (!isServer) {
+        config.resolve.fallback.fs = false;
+      }
+      return config;
+    },
+  };
+  
