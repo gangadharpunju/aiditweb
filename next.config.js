d62d151e99ce6b12123ca23a8a262568a399b2b0
sivananda <cvasia549@gmail.com>
2024-06-28 13:37:15 +0530

    Cybersecurity34 changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['example.com'],
+// /** @type {import('next').NextConfig} */
+
+// const path = require("path")
+
+// const nextConfig = {
+//     reactStrictMode:true,
+//     sassOptions: {
+//         includePaths: [path.join(__dirname, "styles")]
+//     }
+// }
+
+// module.exports = nextConfig
+
+// next.config.js
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Example: Add a rule for CSS modules
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: "style-loader",
+        },
+        {
+          loader: "css-loader",
+          options: {
+            modules: true, 
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
-
-module.exports = nextConfig;
-
-
