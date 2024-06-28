44333e74b8bdc67457afef1a9965376bdb76d2e0
sivananda <cvasia549@gmail.com>
2024-06-28 13:40:53 +0530

    Cybersecurity35 changes

--- next.config.js
+++ next.config.js
@@ -1,35 +1,14 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 
-// const path = require("path")
+const path = require("path")
 
-// const nextConfig = {
-//     reactStrictMode:true,
-//     sassOptions: {
-//         includePaths: [path.join(__dirname, "styles")]
-//     }
-// }
+const nextConfig = {
+    reactStrictMode:true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")]
+    }
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
-// next.config.js
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Example: Add a rule for CSS modules
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        {
-          loader: "style-loader",
-        },
-        {
-          loader: "css-loader",
-          options: {
-            modules: true, 
-          },
-        },
-      ],
-    });
 
-    return config;
-  },
-};
